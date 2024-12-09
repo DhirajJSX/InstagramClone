0b3cf1ffc7d86af2b7caac6e870cddb407ae1d2d
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-12-09 19:33:27 +0530

    done Sign In page

---
+++ src/Data/dataButtons.js
@@ -0,0 +1,112 @@
+import GooglePlay from "./../img/LoginPage/playstore.png"
+import Microsoft from "./../img/LoginPage/microsoft.png"
+
+
+const FooterTerms = [
+    {
+        label: 'Meta',
+        href: '#'
+    },
+    {
+        label: 'About',
+        href: '#'
+    },
+    {
+        label: 'blog',
+        href: '#'
+    },
+    {
+        label: 'Jobs',
+        href: '#'
+    },
+    {
+        label: 'Help',
+        href: '#'
+    },
+    {
+        label: 'Privacy',
+        href: '#'
+    },
+    {
+        label: 'Terms',
+        href: '#'
+    },
+    {
+        label: 'Locations',
+        href: '#'
+    },
+    {
+        label: 'Instagram Lite',
+        href: '#'
+    },
+    {
+        label: 'Threads',
+        href: '#'
+    },
+    {
+        label: 'Contact Uploading & Non-Users',
+        href: '#'
+    },
+    {
+        label: 'Meta Verified',
+        href: '#'
+    }
+];
+
+const Languages = [
+    { label: 'English', value: 'en' },
+    { label: 'Spanish', value: 'es' },
+    { label: 'French', value: 'fr' },
+    { label: 'German', value: 'de' },
+    { label: 'Chinese (Simplified)', value: 'zh-CN' },
+    { label: 'Chinese (Traditional)', value: 'zh-TW' },
+    { label: 'Hindi', value: 'hi' },
+    { label: 'Arabic', value: 'ar' },
+    { label: 'Portuguese', value: 'pt' },
+    { label: 'Russian', value: 'ru' },
+    { label: 'Japanese', value: 'ja' },
+    { label: 'Korean', value: 'ko' },
+    { label: 'Italian', value: 'it' },
+    { label: 'Dutch', value: 'nl' },
+    { label: 'Turkish', value: 'tr' },
+    { label: 'Thai', value: 'th' },
+    { label: 'Swedish', value: 'sv' },
+    { label: 'Greek', value: 'el' },
+    { label: 'Hebrew', value: 'he' },
+    { label: 'Bengali', value: 'bn' },
+    { label: 'Punjabi', value: 'pa' },
+    { label: 'Tamil', value: 'ta' },
+    { label: 'Urdu', value: 'ur' },
+    { label: 'Vietnamese', value: 'vi' },
+    { label: 'Indonesian', value: 'id' },
+    { label: 'Malay', value: 'ms' },
+    { label: 'Polish', value: 'pl' },
+    { label: 'Ukrainian', value: 'uk' },
+    { label: 'Czech', value: 'cs' },
+    { label: 'Hungarian', value: 'hu' },
+    { label: 'Finnish', value: 'fi' },
+    { label: 'Danish', value: 'da' },
+    { label: 'Norwegian', value: 'no' },
+    { label: 'Filipino', value: 'tl' },
+    { label: 'Romanian', value: 'ro' },
+    { label: 'Swahili', value: 'sw' },
+    { label: 'Gujarati', value: 'gu' },
+    { label: 'Marathi', value: 'mr' }
+];
+
+
+const imgButton = [
+    {
+        href: "https://play.google.com/store/apps/details?id=com.instagram.android&pcampaignid=web_share",
+        img: GooglePlay,
+        alt: "Google Play",
+        label: "Download on Google Play"
+    },
+    {
+        href: "https://apps.microsoft.com/detail/9nblggh5l9xt?ocid=webpdpshare",
+        img: Microsoft,
+        alt: "Instagram",
+        label: "Download on the Microsoft Store"
+    }
+]
+export { FooterTerms, Languages, imgButton };
