7987c0770b1ee091ad2b049e0703eab9c08bce55
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-12-15 18:19:07 +0530

    leftSIdebar changes

--- frontend/src/components/HomePage/LeftSidebar.jsx
+++ frontend/src/components/HomePage/LeftSidebar.jsx
@@ -3,18 +3,20 @@ import IGimg from "../../img/LoginPage/instagram.png";
 import { useNavigate } from "react-router-dom";
 import LeftSiderTop from "./LeftSiderButton/LeftSiderTop";
 import LeftSiderButtom from "./LeftSiderButton/LeftSiderButtom";
-function LeftSidebar() {
 
+function LeftSidebar() {
   return (
-    <aside className="w-full  lg:w-[250px] dark:border-gray-700 text-white border-r p-4 pb-10 hidden lg:flex flex-col justify-between lg:sticky lg:top-0 lg:h-screen overflow-hidden">
+    <aside className="w-full lg:max-w-[250px] dark:border-gray-700 text-white border-r p-4 pb-6 hidden lg:flex flex-col justify-between lg:sticky lg:top-0 lg:h-screen overflow-y-auto">
       <div className="flex cursor-pointer items-center ml-5 my-6">
-        <img className="w-[100px]" src={IGimg} alt="Instagram" />
+        <img className="w-[120px] lg:w-[100px] h-auto" src={IGimg} alt="Instagram" />
       </div>
 
       {/* Navigation Sections */}
       <div className="flex flex-col h-full">
-        {/* TOp */}
+        {/* Top section */}
         <LeftSiderTop />
+
+        {/* Bottom section */}
         <LeftSiderButtom />
       </div>
     </aside>
