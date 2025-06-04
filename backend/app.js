3ceb0be97545ec117656f78029131c94c6d64cb2
Dhiraj Bhawsar <139003559+Dhirajbhavsar9900@users.noreply.github.com>
2025-06-04 18:37:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const isProduction = process.env.NODE_ENV === "production";
 
 
